7aea4d8cff0fcc70195d1884b66dc4b313890f96
frnk26 <frankahlennn@gmail.com>
2023-01-18 16:00:57 +0800

    add query

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 
+import { createClient, Provider } from 'urql'
+
+const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API })
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider value={client} >
+      <Component {...pageProps} />
+    </Provider>
+
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+// URQL
+import { PRODUCT_QUERY } from '@/lib/query'
+import { useQuery } from 'urql'
+
 
 export default function Home() {
+  const [results] = useQuery({ query: PRODUCT_QUERY })
+  const { data, fetching, error } = results;
 
+  if (fetching) return <p> loading</p>;
+  if (error) return <p> loading</p>;
+
+  const products = data.products.data;
   return (
     <>
       <Head>
